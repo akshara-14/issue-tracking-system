af9632dd9545b261c39effc36a6049e8d2fe0d19
akshara-14 <akshara.johari.14@gmail.com>
2023-06-08 17:21:32 +0530

    looping through issues to print them

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,9 +1,13 @@
-import Create from "./Create";
-const Details = ({ id }) => {
+const Details = () => {
   const formValuesJson = { ...localStorage };
   console.log(formValuesJson);
   // const formValues = JSON.parse(formValuesJson);
   console.log("data saved");
+  // console.log(Object.keys(formValuesJson));
+  let issues = Object.values(formValuesJson);
+
+  issues.forEach((issues) => console.log(issues));
+
   return <div></div>;
 };
 
